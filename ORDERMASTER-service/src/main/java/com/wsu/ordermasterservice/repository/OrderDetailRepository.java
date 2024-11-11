633ae3a72f944b3ec2ef4d8977d9a4ea868321ec
Robert Sutton <sutton.98@wright.edu>
2024-11-10 23:41:24 -0500

    Update OrderDetailRepository.java

--- ORDERMASTER-service/src/main/java/com/wsu/ordermasterservice/repository/OrderDetailRepository.java
+++ ORDERMASTER-service/src/main/java/com/wsu/ordermasterservice/repository/OrderDetailRepository.java
@@ -1,5 +1,8 @@
 package com.wsu.ordermasterservice.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wsu.ordermasterservice.model.Server;
 
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
