e5579c6c3b30146651b35ae702a98c7a491b6ecb
RomelMiller <156851206+RomelMiller@users.noreply.github.com>
2024-11-10 23:27:21 -0500

    Update FoodOrderRepository.java

--- ORDERMASTER-service/src/main/java/com/wsu/ordermasterservice/repository/FoodOrderRepository.java
+++ ORDERMASTER-service/src/main/java/com/wsu/ordermasterservice/repository/FoodOrderRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wsu.ordermasterservice.model.FoodOrder;
 
-@Repository
 public interface FoodOrderRepository extends JpaRepository<FoodOrder, Integer> {
 
 }
