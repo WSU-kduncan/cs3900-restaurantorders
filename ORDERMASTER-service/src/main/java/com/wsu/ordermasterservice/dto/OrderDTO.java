4fcb58b6b94e67e00633eb90a9a88dc14d14e97a
Robert Sutton <sutton.98@wright.edu>
2024-11-11 00:03:02 -0500

    Update OrderDTO.java

--- ORDERMASTER-service/src/main/java/com/wsu/ordermasterservice/dto/OrderDTO.java
+++ ORDERMASTER-service/src/main/java/com/wsu/ordermasterservice/dto/OrderDTO.java
@@ -1,6 +1,6 @@
 package com.wsu.ordermasterservice.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
