a32815f1b1c2301a1e1c1505b53612ccad964a06
haunspaw <aunspaw.3@wright.edu>
2024-12-01 23:00:02 -0500

    modified:   ORDERMASTER-frontend/package-lock.json 	modified:   ORDERMASTER-frontend/src/app/app.component.html 	modified:   ORDERMASTER-frontend/src/app/app.component.spec.ts 	modified:   ORDERMASTER-frontend/src/app/app.component.ts 	new file:   ORDERMASTER-frontend/src/app/footer/footer.component.css 	new file:   ORDERMASTER-frontend/src/app/footer/footer.component.html 	new file:   ORDERMASTER-frontend/src/app/footer/footer.component.spec.ts 	new file:   ORDERMASTER-frontend/src/app/footer/footer.component.ts 	new file:   ORDERMASTER-frontend/src/app/header/header.component.css 	new file:   ORDERMASTER-frontend/src/app/header/header.component.html 	new file:   ORDERMASTER-frontend/src/app/header/header.component.spec.ts 	new file:   ORDERMASTER-frontend/src/app/header/header.component.ts 	modified:   ORDERMASTER-frontend/src/app/navbar/navbar.component.html 	modified:   ORDERMASTER-frontend/src/app/navbar/navbar.component.ts

--- ORDERMASTER-frontend/src/app/app.component.ts
+++ ORDERMASTER-frontend/src/app/app.component.ts
@@ -2,27 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { RouterOutlet, RouterLink, RouterModule } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterOutlet, RouterLink, RouterModule],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, RouterModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent 
 {
-  title = 'OrderMaster';
-  routerLink = "/"
-  private date: Date = new Date();
-  constructor() 
-  {
-     setInterval(() => {
-       this.date = new Date()
-     }, 1000)
-  }
-  getDate(): Date
-  {
-    return this.date;
-  }
+
 }
--- ORDERMASTER-frontend/src/app/navbar/navbar.component.ts
+++ ORDERMASTER-frontend/src/app/navbar/navbar.component.ts
@@ -25,5 +25,12 @@ export class NavbarComponent
     this.backgroundColor = this.backgroundColor === 'rgb(98, 128, 204)' ? 'rgb(89, 119, 196)' : 'rgb(98, 128, 204)';
   }
 
+  toggleBackground(event: MouseEvent): void {
+    const element = event.target as HTMLElement;
+    const currentColor = getComputedStyle(element).backgroundColor;
+
+    element.style.backgroundColor =
+      currentColor === 'rgb(98, 128, 204)' ? 'rgb(89, 119, 196)' : 'rgb(98, 128, 204)';
+  }
   //TODO: animations for actually being clicked
 }
