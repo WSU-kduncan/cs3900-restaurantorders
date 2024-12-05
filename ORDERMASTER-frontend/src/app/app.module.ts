35b575e9a313be16a0e6a016ed926e4dcca2cdc6
Denver Woolard <112095324+Ciriden@users.noreply.github.com>
2024-12-05 13:46:57 -0500

    Updating Servers Component

--- ORDERMASTER-frontend/src/app/app.module.ts
+++ ORDERMASTER-frontend/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { OrderService } from './services/order.service/order.service';
 
 // @NgModule decorator with its metadata
 @NgModule({
   declarations: [],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [OrderService],
   bootstrap: []
 })
